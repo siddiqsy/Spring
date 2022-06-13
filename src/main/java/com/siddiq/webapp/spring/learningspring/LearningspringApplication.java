74e2d744ee3419fea5e22b3cc5e0669e4fb14ed7
Siddiq Syed <siddiqsy@buffalo.edu>
2022-06-13 14:57:24 -0700

    #Spring

---
+++ src/main/java/com/siddiq/webapp/spring/learningspring/LearningspringApplication.java
@@ -0,0 +1,13 @@
+package com.siddiq.webapp.spring.learningspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningspringApplication.class, args);
+	}
+
+}
